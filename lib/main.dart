767c504e7f1b36a18955088f3509a4609fd897e7
Jasraj Singh <jasraj001@e.ntu.edu.sg>
2021-03-13 13:18:30 +0800

    changed images, pubspec, app and nav bars

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/view/CustomRadioButton.dart';
 import 'package:flutterapp/view/HomePage.dart';
-import 'package:flutterapp/view/LogbookPage.dart';
 import 'package:flutterapp/view/Medication.dart';
+import 'package:flutterapp/view/ExercisePage.dart';
 
 void main() {
   runApp(HomePage());
-  // runApp(LogBookPage());
   // runApp(MedicationPage());
+  // runApp(ExercisePage());
 }
