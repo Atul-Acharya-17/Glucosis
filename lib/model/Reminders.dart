239d061c1afce8c5b70399feea9ac4d3735dffe8
Jasraj Singh <jasraj001@e.ntu.edu.sg>
2021-03-15 13:11:41 +0800

    Added user and reminders

---
+++ lib/model/Reminders.dart
@@ -0,0 +1,8 @@
+class Reminders {
+  var medicines; // List<String>
+  var dosage; // List<String>
+  var days; // List<List<String>>
+  var timings; // List<List<String>>
+
+  Reminders({this.medicine, this.dosage, this,days, this.timings});
+}
---
+++ lib/model/User.dart
@@ -0,0 +1,17 @@
+class User {
+  String email;
+  String phoneNumber;
+  String diabetesType;
+  String location;
+  double weight;
+  double height;
+  // attribute?
+
+  User(
+      {this.email,
+      this.phoneNumber,
+      this.diabetesType,
+      this.location,
+      this.weight,
+      this.height});
+}
