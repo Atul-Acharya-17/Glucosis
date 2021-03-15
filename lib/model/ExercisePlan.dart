a07fef60fe2ac85cf40bdc46aee4ed9632050adb
Srishti <srishti.arora72@gmail.com>
2021-03-15 14:28:31 +0800

    exercisePlan and mealPlan entity

---
+++ lib/model/ExercisePlan.dart
@@ -0,0 +1,13 @@
+class ExercisePlan {
+  int targetExTime;
+  int achievedExTime;
+  String exLevel;
+  var recommendationTable;
+
+  ExercisePlan(
+    this.targetExTime,
+    this.achievedExTime,
+    this.exLevel,
+    this.recommendationTable,
+  );
+}
---
+++ lib/model/MealPlan.dart
@@ -0,0 +1,13 @@
+class MealPlan {
+  String dietaryPref;
+  int targetCarbs;
+  int targetSugar;
+  String allergies;
+
+  MealPlan(
+    this.dietaryPref,
+    this.targetCarbs,
+    this.targetSugar,
+    this.allergies,
+  );
+}
