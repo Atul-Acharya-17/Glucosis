c6eae117349b077ebfc75c437b69d9dcc85dbd46
kimtxn <kimberley028@gmail.com>
2021-04-05 20:50:03 +0800

    Added user attributes

--- lib/model/User.dart
+++ lib/model/User.dart
@@ -16,6 +16,9 @@ class User {
   String _foodPreference;
   String _gender;
   int _targetCalories;
+  int _targetCarbs;
+  int _minGlucose;
+  int _maxGlucose;
   DateTime _dateOfBirth;
 
   User(
@@ -31,6 +34,9 @@ class User {
       String foodPreference,
       String gender,
       int targetCalories,
+      int targetCarbs,
+      int minGlucose,
+      int maxGlucose,
       DateTime dateOfBirth})
       : _name = name,
         _email = email,
@@ -44,79 +50,100 @@ class User {
         _foodPreference = foodPreference,
         _gender = gender,
         _targetCalories = targetCalories,
+        _targetCarbs = targetCarbs,
+        _minGlucose = minGlucose,
+        _maxGlucose = maxGlucose,
         _dateOfBirth = dateOfBirth;
 
   get name => _name;
+
   get height => _height;
+
   get weight => _weight;
+
   get location => _location;
+
   get diabetesType => _diabetesType;
+
   get phoneNumber => _phoneNumber;
+
   get email => _email;
+
   get dietRestrictions => _dietRestrictions;
+
   get exercisePreference => _exercisePreference;
+
   get foodPreference => _foodPreference;
+
   get gender => _gender;
+
   get targetCalories => _targetCalories;
+
+  get targetCarbs => _targetCarbs;
+
+  get minGlucose => _minGlucose;
+
+  get maxGlucose => _maxGlucose;
+
   get dateOfBirth => _dateOfBirth;
 
-   set setName(String name)
-  {
-    _name=name;
+  set setName(String name) {
+    _name = name;
   }
 
-   set setHeight(double height)
-  {
-    _height=height;
+  set setHeight(double height) {
+    _height = height;
   }
 
-  set setWeight(double weight)
-  {
-    _weight=weight;
+  set setWeight(double weight) {
+    _weight = weight;
   }
 
-  set setLocation(String location)
-  {
-    _location=location;
+  set setLocation(String location) {
+    _location = location;
   }
 
-  set setPhoneNumber(String phoneNumber)
-  {
-    _phoneNumber=phoneNumber;
+  set setPhoneNumber(String phoneNumber) {
+    _phoneNumber = phoneNumber;
   }
 
-  set setGender(String gender)
-  {
-    _gender=gender;
+  set setGender(String gender) {
+    _gender = gender;
   }
 
-  set setdietRestrictions(List<String> dietaryRestrictions)
-  {
-    _dietRestrictions=dietaryRestrictions;
+  set setdietRestrictions(List<String> dietaryRestrictions) {
+    _dietRestrictions = dietaryRestrictions;
   }
 
-  set setFoodPreference(String foodPreference)
-  {
-    _foodPreference=foodPreference;
-  }
- 
-  set setExercisePreference(String exerPreference)
-  {
-    _exercisePreference=exerPreference;
+  set setFoodPreference(String foodPreference) {
+    _foodPreference = foodPreference;
   }
 
-  set setDiabetesType(String diabetesType)
-  {
-    _diabetesType=diabetesType;
+  set setExercisePreference(String exerPreference) {
+    _exercisePreference = exerPreference;
   }
-  
 
-  set setTargetCalories(int calories)
-  {
-    _targetCalories=calories;
+  set setDiabetesType(String diabetesType) {
+    _diabetesType = diabetesType;
   }
-  set setDob(DateTime dateOfBirth)
-  {
-    _dateOfBirth=dateOfBirth;
+
+  set setTargetCalories(int calories) {
+    _targetCalories = calories;
+  }
+
+  set setTargetCarbs(int carbs) {
+    _targetCarbs = carbs;
+  }
+
+  set setMinTargetGlucose(int minGlucose) {
+    _minGlucose = minGlucose;
+  }
+
+  set setMaxTargetGlucose(int maxGlucose) {
+    _maxGlucose = maxGlucose;
+  }
+
+  set setDob(DateTime dateOfBirth) {
+    _dateOfBirth = dateOfBirth;
   }
 }
