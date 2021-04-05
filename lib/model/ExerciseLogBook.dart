983a31ff7a37845f13cdd1cb6153f5b41364c0e8
kimtxn <kimberley028@gmail.com>
2021-04-05 21:47:50 +0800

    Modified logbook and record related classes

--- lib/model/ExerciseLogBook.dart
+++ lib/model/ExerciseLogBook.dart
@@ -1,23 +1,34 @@
 import '../model/ExerciseRecord.dart';
 import '../model/Data.dart';
 
+/// Log book collection entity containing a user's past exercise records.
 class ExerciseLogBook {
-  List<ExerciseRecord> exerciseRecordsList;
+  List<ExerciseRecord> _exerciseRecordsList;
 
   ExerciseLogBook({
-    this.exerciseRecordsList,
-  });
+    exerciseRecordsList,
+  }) : _exerciseRecordsList = exerciseRecordsList;
 
+  get exerciseRecordsList => _exerciseRecordsList;
+
+  /// Gets data to create chart on home page.
   List<Data> dataHomePage() {
     List<Data> chartData = logBookToData();
     return chartData.sublist(0, 7);
   }
 
+  /// Gets data to create chart on log book page.
   List<Data> dataLogBookPage() {
     List<Data> chartData = logBookToData();
     return chartData;
   }
 
+  /// Add exercise record.
+  void addRecord(ExerciseRecord exerciseRecord) {
+    _exerciseRecordsList.add(exerciseRecord);
+  }
+
+  /// Get exercise records history.
   List<Data> logBookToData() {
     List<Data> chartData;
     for (int i = 0; i < exerciseRecordsList.length; i++) {
--- lib/model/FoodLogBook.dart
+++ lib/model/FoodLogBook.dart
@@ -1,23 +1,34 @@
 import '../model/FoodRecord.dart';
 import '../model/Data.dart';
 
+/// Collection entity containing past food entries of a user.
 class FoodLogBook {
-  List<FoodRecord> foodRecordsList;
+  List<FoodRecord> _foodRecordsList;
 
   FoodLogBook({
-    this.foodRecordsList,
-  });
+    foodRecordsList,
+  }) : _foodRecordsList = foodRecordsList;
 
+  get foodRecordsList => _foodRecordsList;
+
+  /// Gets data to create chart on home page.
   List<Data> dataHomePage() {
     List<Data> chartData = logBookToData();
     return chartData.sublist(0, 7);
   }
 
+  /// Gets data to create chart on log book page.
   List<Data> dataLogBookPage() {
     List<Data> chartData = logBookToData();
     return chartData;
   }
 
+  /// Add food record.
+  void addRecord(FoodRecord foodRecord) {
+    _foodRecordsList.add(foodRecord);
+  }
+
+  /// Get food records history.
   List<Data> logBookToData() {
     List<Data> chartData;
     for (int i = 0; i < foodRecordsList.length; i++) {
