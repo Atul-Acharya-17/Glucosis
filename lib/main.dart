a05bafc7aa4589244795f09ed35b26a807fc35a7
Atul Acharya <61643403+Atul-Acharya-17@users.noreply.github.com>
2021-04-12 03:18:45 +0800

    Delete main.dart

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'view/LoginPage.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(LoginPage());
-}
